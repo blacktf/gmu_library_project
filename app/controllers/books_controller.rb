95b3b2f2dedfd50097d495832368010d9b7252aa
Al Everett <al.everett@evanschambers.com>
2015-05-06 19:19:04 -0700

    prevent deletion by non-admin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -78,10 +78,14 @@ class BooksController < ApplicationController
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+    if @current_user.admin
+      @book.destroy
+      respond_to do |format|
+        format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to @book
     end
   end
 
