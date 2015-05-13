8b9ae1966e4c52d658c122e79be530032862cdbe
Tim Black <blacktf@gmail.com>
2015-05-13 16:40:49 -0700

    Added validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,15 @@ class Book < ActiveRecord::Base
   belongs_to :author
   has_many :reservations, dependent: :destroy
 
+  validates :title, :isbn, :author, :pages,
+   :genre, :abstract, :published_on,
+   :total_in_library, presence: true
+
+   validates :total_in_library, :pages,
+    numericality: { only_integer: true,
+    greater_than_or_equal_to: 0 }
+
+    validates :abstract, length: { minimum: 15 }
 
  def self.search(query)
     # search by title , author or isbn
